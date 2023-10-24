6e83823898f9d19a53a98b58e854134b01c53539
zxcola <zxcola1@gmail.com>
2023-10-24 11:18:14 +0300

    init commit

---
+++ src/main/java/by/bsu/rfict/WarehouseAccountingSystem/WarehouseAccountingSystemApplication.java
@@ -0,0 +1,13 @@
+package by.bsu.rfict.WarehouseAccountingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseAccountingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarehouseAccountingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/bsu/rfict/WarehouseAccountingSystem/WarehouseAccountingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package by.bsu.rfict.WarehouseAccountingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseAccountingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
