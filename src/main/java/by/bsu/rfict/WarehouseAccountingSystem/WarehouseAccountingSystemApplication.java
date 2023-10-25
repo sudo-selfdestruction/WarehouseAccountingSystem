c9d8fd8927153bd175999b7f586be96fb92c9b47
zxcola <zxcola1@gmail.com>
2023-10-25 12:39:46 +0300

    change: fix

--- src/main/java/by/bsu/rfict/WarehouseAccountingSystem/WarehouseAccountingSystemApplication.java
+++ src/main/java/by/bsu/rfict/WarehouseAccountingSystem/WarehouseAccountingSystemApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WarehouseAccountingSystemApplication {
 
 	public static void main(String[] args) {
-		System.out.println("da");
 		SpringApplication.run(WarehouseAccountingSystemApplication.class, args);
 	}
 
