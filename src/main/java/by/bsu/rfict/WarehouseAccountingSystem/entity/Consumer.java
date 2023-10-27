74cf9bec61c1b3235b899fa9e8c8a242a23d79a5
sudo-selfdestruction <eaa040104@gmail.com>
2023-10-27 10:52:20 +0300

    add: consumer class

--- src/main/java/by/bsu/rfict/WarehouseAccountingSystem/entity/Consumer.java
+++ src/main/java/by/bsu/rfict/WarehouseAccountingSystem/entity/Consumer.java
@@ -1,5 +1,15 @@
 package by.bsu.rfict.WarehouseAccountingSystem.entity;
 
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Data
 public class Consumer {
+
+    @Id
+    private Long id;
 
 }
