df506310ebd11fbd444435246a2e999da53690e0
Arseni Zhirnevich <szhirnevich@mail.ru>
2023-10-27 18:03:07 +0300

    minor changes

--- src/main/java/by/bsu/rfict/WarehouseAccountingSystem/entity/Role.java
+++ src/main/java/by/bsu/rfict/WarehouseAccountingSystem/entity/Role.java
@@ -1,15 +1,17 @@
 package by.bsu.rfict.WarehouseAccountingSystem.entity;
 
 import lombok.*;
-
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Data
+@Table(name = "roles")
 public class Role {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @Column(name = "name")
+    private String name;
 }
