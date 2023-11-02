f7957aa7f99fbe43f6214bf037df6a40948851a6
Arseni Zhirnevich <szhirnevich@mail.ru>
2023-11-02 21:08:26 +0300

    create: entity Item

---
+++ src/main/java/by/bsu/rfict/WarehouseAccountingSystem/entity/Item.java
@@ -0,0 +1,30 @@
+package by.bsu.rfict.WarehouseAccountingSystem.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "items")
+@Data
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description", columnDefinition = "text")
+    private String description;
+
+    @Column(name = "manufacturing_country")
+    private String manufacturingCountry;
+
+    @Column(name = "cost")
+    private int cost;
+
+    @Column(name = "weight")
+    private short weight;
+}
