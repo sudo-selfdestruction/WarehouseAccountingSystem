d30a4a17364cde727bad4f2e4e323f609364b1eb
Arseni Zhirnevich <szhirnevich@mail.ru>
2023-11-02 20:30:45 +0300

    create: entity User

---
+++ src/main/java/by/bsu/rfict/WarehouseAccountingSystem/entity/User.java
@@ -0,0 +1,30 @@
+package by.bsu.rfict.WarehouseAccountingSystem.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "phoneNumber")
+    private String phoneNumber;
+}
