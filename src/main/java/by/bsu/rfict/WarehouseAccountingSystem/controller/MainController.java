e6a903d8091e6b6ab37898b85c4e0c12c80c0c4f
sudo-selfdestruction <eaa040104@gmail.com>
2023-11-03 12:52:37 +0300

    add: main controller

--- src/main/java/by/bsu/rfict/WarehouseAccountingSystem/controller/MainController.java
+++ src/main/java/by/bsu/rfict/WarehouseAccountingSystem/controller/MainController.java
@@ -1,12 +1,15 @@
 package by.bsu.rfict.WarehouseAccountingSystem.controller;
 
+import by.bsu.rfict.WarehouseAccountingSystem.entity.Order;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class MainController {
     @GetMapping("/")
-    public void getMainPage() {
-
+    public String getMainPage() {
+        return "Main";
     }
 }
