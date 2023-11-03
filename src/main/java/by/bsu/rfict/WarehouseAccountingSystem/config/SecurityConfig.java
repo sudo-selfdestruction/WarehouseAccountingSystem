9699509b621b328c1821609e6afa9bee84d0af22
sudo-selfdestruction <eaa040104@gmail.com>
2023-11-03 22:18:29 +0300

    refactor: minor

--- src/main/java/by/bsu/rfict/WarehouseAccountingSystem/config/SecurityConfig.java
+++ src/main/java/by/bsu/rfict/WarehouseAccountingSystem/config/SecurityConfig.java
@@ -1,8 +1,6 @@
 package by.bsu.rfict.WarehouseAccountingSystem.config;
 
-import by.bsu.rfict.WarehouseAccountingSystem.repository.UserRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,8 +12,6 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 @AllArgsConstructor
 public class SecurityConfig {
-    @Autowired
-    private UserRepository repository;
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
