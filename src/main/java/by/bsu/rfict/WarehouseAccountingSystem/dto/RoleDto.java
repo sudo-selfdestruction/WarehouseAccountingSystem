382fd5cfa67936de9ae3c11a2a8c8c52accef5ae
sudo-selfdestruction <eaa040104@gmail.com>
2023-11-03 22:37:16 +0300

    add: DTOs

---
+++ src/main/java/by/bsu/rfict/WarehouseAccountingSystem/dto/RoleDto.java
@@ -0,0 +1,9 @@
+package by.bsu.rfict.WarehouseAccountingSystem.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class RoleDto {
+}
