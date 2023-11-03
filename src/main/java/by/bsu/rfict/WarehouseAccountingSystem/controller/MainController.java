c108888f0c1caad3df396f075ae879c08a9e5681
sudo-selfdestruction <eaa040104@gmail.com>
2023-11-03 22:17:28 +0300

    refactor: minor

--- src/main/java/by/bsu/rfict/WarehouseAccountingSystem/controller/MainController.java
+++ src/main/java/by/bsu/rfict/WarehouseAccountingSystem/controller/MainController.java
@@ -1,6 +1,5 @@
 package by.bsu.rfict.WarehouseAccountingSystem.controller;
 
-import by.bsu.rfict.WarehouseAccountingSystem.entity.Order;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
