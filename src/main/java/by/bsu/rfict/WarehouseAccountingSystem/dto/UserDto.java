668ee48214caa8df7dd0e9b3e0ed21460499f443
Arseni Zhirnevich <szhirnevich@mail.ru>
2023-11-09 22:08:04 +0300

    add: dto realization

--- src/main/java/by/bsu/rfict/WarehouseAccountingSystem/dto/UserDto.java
+++ src/main/java/by/bsu/rfict/WarehouseAccountingSystem/dto/UserDto.java
@@ -1,9 +1,37 @@
 package by.bsu.rfict.WarehouseAccountingSystem.dto;
 
+import by.bsu.rfict.WarehouseAccountingSystem.entity.User;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class UserDto {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private String password;
+
+    public static UserDto createUserDtoFromUser(User user) {
+        UserDto userDto = new UserDto();
+        userDto.setFirstName(user.getFirstName());
+        userDto.setLastName(user.getLastName());
+        userDto.setEmail(user.getEmail());
+        userDto.setPhoneNumber(user.getPhoneNumber());
+        userDto.setPassword(user.getPassword());
+
+        return userDto;
+    }
+
+    public static User createUserFromUserDto(UserDto userDto) {
+        User user = new User();
+        user.setFirstName(userDto.getFirstName());
+        user.setLastName(userDto.getLastName());
+        user.setEmail(userDto.getEmail());
+        user.setPhoneNumber(userDto.getPhoneNumber());
+        user.setPassword(userDto.getPassword());
+
+        return user;
+    }
 }
