480402d35bca50cc024bb344ee23ab8408188dba
Arseni Zhirnevich <szhirnevich@mail.ru>
2023-11-09 19:15:24 +0300

    new: controller

---
+++ src/main/java/by/bsu/rfict/WarehouseAccountingSystem/controller/AuthenticationController.java
@@ -0,0 +1,9 @@
+package by.bsu.rfict.WarehouseAccountingSystem.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class AuthenticationController {
+}
