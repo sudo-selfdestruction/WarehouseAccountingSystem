64913eaf4bdc714af4d26995b627357947d040f3
sudo-selfdestruction <eaa040104@gmail.com>
2023-11-20 13:12:23 +0300

    change: OrderDTO

--- src/main/java/by/bsu/rfict/WarehouseAccountingSystem/dto/OrderDto.java
+++ src/main/java/by/bsu/rfict/WarehouseAccountingSystem/dto/OrderDto.java
@@ -1,9 +1,22 @@
 package by.bsu.rfict.WarehouseAccountingSystem.dto;
 
+import by.bsu.rfict.WarehouseAccountingSystem.entity.Item;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class OrderDto {
+    private List<Item> itemList;
+    private double cost;
+    private double weight;
 }
