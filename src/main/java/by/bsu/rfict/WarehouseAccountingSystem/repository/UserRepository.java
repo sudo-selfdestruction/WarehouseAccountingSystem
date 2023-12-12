7e3471ba384301e585dbcd4a91741bc15f294c50
sudo-selfdestruction <eaa040104@gmail.com>
2023-12-12 23:37:39 +0300

    change: User repository

--- src/main/java/by/bsu/rfict/WarehouseAccountingSystem/repository/UserRepository.java
+++ src/main/java/by/bsu/rfict/WarehouseAccountingSystem/repository/UserRepository.java
@@ -2,9 +2,12 @@ package by.bsu.rfict.WarehouseAccountingSystem.repository;
 
 import by.bsu.rfict.WarehouseAccountingSystem.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findUserByEmail(String email);
+    @Query(nativeQuery = true, value = "SELECT * FROM user WHERE username = :login")
+    User getUserByLogin(String login);
 }
