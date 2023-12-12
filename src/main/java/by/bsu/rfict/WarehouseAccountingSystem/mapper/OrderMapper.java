31353803e4866539935e0eb9ead934b3f05a487e
sudo-selfdestruction <eaa040104@gmail.com>
2023-12-12 23:38:36 +0300

    change: Order mapper

--- src/main/java/by/bsu/rfict/WarehouseAccountingSystem/mapper/OrderMapper.java
+++ src/main/java/by/bsu/rfict/WarehouseAccountingSystem/mapper/OrderMapper.java
@@ -1,22 +1,30 @@
 package by.bsu.rfict.WarehouseAccountingSystem.mapper;
 
 import by.bsu.rfict.WarehouseAccountingSystem.dto.OrderDto;
+import by.bsu.rfict.WarehouseAccountingSystem.entity.Item;
 import by.bsu.rfict.WarehouseAccountingSystem.entity.Order;
 import by.bsu.rfict.WarehouseAccountingSystem.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
+import java.util.List;
+
+import static by.bsu.rfict.WarehouseAccountingSystem.mapper.ItemMapper.toItemList;
 
 @Component
 public class OrderMapper {
     @Autowired
-    OrderService orderService;
+    private OrderService orderService;
+
     public Order toOrder(OrderDto orderDto) {
+        List<Item> itemList = toItemList(orderDto);
         return Order.builder()
                 .order_date(LocalDate.now())
-                .weight(orderService.calcWeight(orderDto))
-                .cost(orderService.calcCost(orderDto))
+                .itemList(itemList)
+                .weight(orderService.calcWeight(itemList))
+                .cost(orderService.calcCost(itemList))
+                .description(orderDto.getDescription())
                 .build();
     } 
 }
