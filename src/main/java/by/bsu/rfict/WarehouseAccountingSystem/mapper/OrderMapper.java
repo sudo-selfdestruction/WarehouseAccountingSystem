8fe62b1f8e1632cf07c802ea559d5933ceda9e8b
sudo-selfdestruction <eaa040104@gmail.com>
2023-12-14 23:03:55 +0300

    change: order mapper

--- src/main/java/by/bsu/rfict/WarehouseAccountingSystem/mapper/OrderMapper.java
+++ src/main/java/by/bsu/rfict/WarehouseAccountingSystem/mapper/OrderMapper.java
@@ -10,21 +10,22 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDate;
 import java.util.List;
 
-import static by.bsu.rfict.WarehouseAccountingSystem.mapper.ItemMapper.toItemList;
-
 @Component
 public class OrderMapper {
     @Autowired
     private OrderService orderService;
+    @Autowired
+    private ItemMapper itemMapper;
 
     public Order toOrder(OrderDto orderDto) {
-        List<Item> itemList = toItemList(orderDto);
+        List<Item> itemList = itemMapper.toItemList(orderDto);
         return Order.builder()
                 .order_date(LocalDate.now())
                 .itemList(itemList)
                 .weight(orderService.calcWeight(itemList))
                 .cost(orderService.calcCost(itemList))
                 .description(orderDto.getDescription())
+                .storage_period(7)
                 .build();
     } 
 }
