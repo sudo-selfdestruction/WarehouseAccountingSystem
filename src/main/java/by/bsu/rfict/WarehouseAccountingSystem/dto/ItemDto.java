b3b1c5f926ba834bf6fba575adfa56bd3afd640b
sudo-selfdestruction <eaa040104@gmail.com>
2023-12-14 23:48:08 +0300

    change: item DTO

--- src/main/java/by/bsu/rfict/WarehouseAccountingSystem/dto/ItemDto.java
+++ src/main/java/by/bsu/rfict/WarehouseAccountingSystem/dto/ItemDto.java
@@ -1,9 +1,7 @@
 package by.bsu.rfict.WarehouseAccountingSystem.dto;
 
-import by.bsu.rfict.WarehouseAccountingSystem.entity.Item;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
-import lombok.extern.slf4j.Slf4j;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -14,14 +12,4 @@ public class ItemDto {
     private int cost;
     private double weight;
 
-    public static ItemDto createItemDtoFromItem(Item item) {
-        ItemDto itemDto = new ItemDto();
-        itemDto.setName(item.getName());
-        itemDto.setDescription(item.getDescription());
-        itemDto.setManufacturingCountry(item.getManufacturingCountry());
-        itemDto.setCost(item.getCost());
-        itemDto.setWeight(item.getWeight());
-
-        return itemDto;
-    }
 }
