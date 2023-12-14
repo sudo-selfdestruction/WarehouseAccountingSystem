2803f4534332b6b6a8c81ed6a833d94f4b0314d9
sudo-selfdestruction <eaa040104@gmail.com>
2023-12-14 23:48:20 +0300

    change: item mapper

--- src/main/java/by/bsu/rfict/WarehouseAccountingSystem/mapper/ItemMapper.java
+++ src/main/java/by/bsu/rfict/WarehouseAccountingSystem/mapper/ItemMapper.java
@@ -31,4 +31,29 @@ public class ItemMapper {
         }
         return itemList;
     }
+
+    public ItemDto toItemDto(Item item) {
+        ItemDto itemDto = new ItemDto();
+        itemDto.setName(item.getName());
+        itemDto.setDescription(item.getDescription());
+        itemDto.setManufacturingCountry(item.getManufacturingCountry());
+        itemDto.setCost(item.getCost());
+        itemDto.setWeight(item.getWeight());
+        return itemDto;
+    }
+
+    public List<ItemDto> toItemDtoList(List<Item> itemList) {
+        List<ItemDto> itemDtoList = new ArrayList<ItemDto>();
+        for (Item item : itemList) {
+            ItemDto itemDto = new ItemDto();
+            itemDto.setName(item.getName());
+            itemDto.setDescription(item.getDescription());
+            itemDto.setManufacturingCountry(item.getManufacturingCountry());
+            itemDto.setCost(item.getCost());
+            itemDto.setWeight(item.getWeight());
+            itemDtoList.add(itemDto);
+        }
+        return itemDtoList;
+    }
+
 }
