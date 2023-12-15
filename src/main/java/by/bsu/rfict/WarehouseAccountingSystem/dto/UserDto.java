beb22a1fae08bc4519f51804a2a404eb55b4e8be
sudo-selfdestruction <eaa040104@gmail.com>
2023-12-15 16:42:39 +0300

    change: user DTO

--- src/main/java/by/bsu/rfict/WarehouseAccountingSystem/dto/UserDto.java
+++ src/main/java/by/bsu/rfict/WarehouseAccountingSystem/dto/UserDto.java
@@ -12,26 +12,4 @@ public class UserDto {
     private String email;
     private String phoneNumber;
     private String password;
-
-    public static UserDto createUserDtoFromUser(User user) {
-        UserDto userDto = new UserDto();
-        userDto.setFirstName(user.getFirstName());
-        userDto.setLastName(user.getLastName());
-        userDto.setEmail(user.getEmail());
-        userDto.setPhoneNumber(user.getPhoneNumber());
-        userDto.setPassword(user.getPassword());
-
-        return userDto;
-    }
-
-    public static User createUserFromUserDto(UserDto userDto) {
-        User user = new User();
-        user.setFirstName(userDto.getFirstName());
-        user.setLastName(userDto.getLastName());
-        user.setEmail(userDto.getEmail());
-        user.setPhoneNumber(userDto.getPhoneNumber());
-        user.setPassword(userDto.getPassword());
-
-        return user;
-    }
 }
